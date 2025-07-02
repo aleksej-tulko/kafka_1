9964aa5f0c040d60270278e9da9e3e642f1a0dc2
Aleix Tulko <alexeitulko@gmail.com>
2025-07-02 19:44:03 +0200

    while

--- app/messages.py
+++ app/messages.py
@@ -29,11 +29,11 @@ def create_message(incr_num: int) -> None:
 
 incr_num = 0
 
-while True:
-    try:
+try:
+    while True:
         Thread(target=create_message(incr_num=incr_num))
         incr_num += 1
-    except Exception as ex:
-        raise RuntimeError(ex)
-    finally:
-        sleep(10)
+except Exception as ex:
+    raise RuntimeError(ex)
+finally:
+    sleep(10)

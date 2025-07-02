3b07234266b37b3490f70e2dc58609f049e22329
Aleix Tulko <alexeitulko@gmail.com>
2025-07-02 20:05:29 +0200

    infinite loops

--- app/messages.py
+++ app/messages.py
@@ -1,10 +1,9 @@
 from time import sleep
 from threading import Thread
-from confluent_kafka import Consumer, Producer
+from confluent_kafka import Consumer, Producer, KafkaException
 
 conf = {
-    "bootstrap.servers":
-    "192.168.1.129:9093,192.168.1.129:9095,192.168.1.129:9097",
+    "bootstrap.servers": "192.168.1.129:9093,192.168.1.129:9095,192.168.1.129:9097",
 }
 
 base_consumer_conf = conf | {"auto.offset.reset": "earliest"}
@@ -15,30 +14,57 @@ producer = Producer(conf)
 single_message_consumer = Consumer(single_message_conf)
 batch_consumer = Consumer(batch_conf)
 
+TOPIC = 'pract-task'
+
+
+def consume_infinite_loop(consumer: Consumer) -> None:
+    consumer.subscribe([TOPIC])
+    try:
+        while True:
+            msg = consumer.poll(0.1)
+
+            if msg is None:
+                continue
+            if msg.error():
+                print(f"Ошибка: {msg.error()}")
+                continue
+
+            key = msg.key().decode("utf-8")
+            value = msg.value().decode("utf-8")
+            print(
+                f"Получено сообщение: {key=}, {value=}, offset={msg.offset()}"
+            )
+    finally:
+        consumer.close()
+
 
 def create_message(incr_num: int) -> None:
     producer.produce(
-        topic='pract-task',
+        topic=TOPIC,
         key=f'key-{incr_num}',
         value=f'message-{incr_num}',
     )
-    producer.flush()
 
 
-def producer_thread(num):
-    Thread(target=create_message, args=(num,)).start()
+def producer_infinite_loop():
+    incr_num = 0
+    try:
+        while True:
+            create_message(incr_num)
+            incr_num += 1
+            if incr_num % 10 == 0:
+                producer.flush()
+            sleep(0.1)
+    finally:
+        producer.flush()
+        producer.close()
+        print("Producer closed")
 
 
-incr_num = 0
+if __name__ == "__main__":
+    t1 = Thread(target=consume_infinite_loop, args=(single_message_consumer, "SingleConsumer"), daemon=True)
+    t2 = Thread(target=consume_infinite_loop, args=(batch_consumer, "BatchConsumer"), daemon=True)
+    t1.start()
+    t2.start()
 
-try:
-    while True:
-        producer_thread(incr_num)
-        incr_num += 1
-        sleep(0.1)
-except Exception as ex:
-    raise RuntimeError(ex)
-finally:
-    sleep(10)
-    producer.flush()
-    producer.close()
+    producer_infinite_loop()

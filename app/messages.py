d87eea0e0c6acad2081d1f57d5400fa830430e4e
Aleix Tulko <alexeitulko@gmail.com>
2025-07-02 19:26:05 +0200

    consumer

--- app/messages.py
+++ app/messages.py
@@ -1,22 +1,33 @@
 from time import sleep
 from threading import Thread
 
-from confluent_kafka import Producer
+from confluent_kafka import Consumer, Producer
 
 conf = {
-    "bootstrap.servers": "192.168.1.129:9093",
+    "bootstrap.servers":
+    "192.168.1.129:9093,192.168.1.129:9095,192.168.1.129:9097",
 }
 
+producer_conf = conf
+consumer_conf = conf.update({"auto.offset.reset": "earliest"})
+
 producer = Producer(conf)
+SingleMessageConsumer = Consumer(
+    consumer_conf.update({"group.id": "single_msg_group"})
+)
+BatchMessageConsumer = Consumer(
+    consumer_conf.update({"group.id": "batch_group"})
+)
 
 
 def create_message(incr_num: int) -> None:
     producer.produce(
-        topic='some-topic',
+        topic='pract-task',
         key=f'key-{incr_num}',
         value=f'message-{incr_num}',
     )
     producer.flush()
+    producer.close()
 
 
 incr_num = 0

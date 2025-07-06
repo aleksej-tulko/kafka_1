64d0878a9862a5ad26e3258b2649e88eabed47de
Aleix Tulko <alexeitulko@gmail.com>
2025-07-06 17:48:19 +0200

    str

--- app/messages.py
+++ app/messages.py
@@ -51,9 +51,7 @@ json_serializer = JSONSerializer(json_schema_str, schema_registry_client)
 key_serializer = StringSerializer('utf_8')
 value_serializer = json_serializer
 
-json_deserializer = JSONDeserializer(
-    json_schema_str,
-    schema_registry_client)
+json_deserializer = JSONDeserializer(scheme_str=json_schema_str)
 
 
 conf = {
